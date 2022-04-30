f646fbf6fb79a83ff450903d0296cd6e522615c6
sirisha469 <chsiri469@gmail.com>
2022-04-30 19:23:46 +0530

    First-Commit

---
+++ src/components/Home/TypeWriter.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function TypeWriter() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Full Stack Web Developer",
+          "MERN Stack Developer",
+          "Open Source Contributor",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default TypeWriter;
